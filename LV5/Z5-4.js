e68d212c51ca4e5ed06835a9e43e45e6ccd6292f
Marin Valenčak <valencakmarin@gmail.com>
2026-05-08 02:43:42 +0200

    feat(LV5): solve the fourth problem

---
+++ LV5/Z5-4.js
@@ -0,0 +1,147 @@
+var width = 960;
+var height = 700;
+
+var centered;
+
+var projection = d3.geo.mercator()
+    .center([16, 45.5])
+    .scale(5000)
+    .translate([width / 2, height / 2]);
+
+var path = d3.geo.path()
+    .projection(projection);
+
+var zoom = d3.behavior.zoom()
+    .scaleExtent([1, 8])
+    .on("zoom", zoomed);
+
+var svg = d3.select("body")
+    .append("svg")
+    .attr("width", width)
+    .attr("height", height)
+    .call(zoom);
+
+var g = svg.append("g");
+
+var tooltip = d3.select("body")
+    .append("div")
+    .style("position", "absolute")
+    .style("padding", "8px")
+    .style("background", "black")
+    .style("color", "white")
+    .style("border-radius", "5px")
+    .style("pointer-events", "none")
+    .style("opacity", 0);
+
+var details = d3.select('body')
+    .append("div")
+    .style('position', "absolute")
+    .style('margin-top', "20px")
+    .style('top', "20px")
+    .style('right', "20px")
+    .style('background', "white")
+    .style('padding', "15px")
+    .style('border', "1px solid black");
+
+var color = d3.scale
+    .linear()
+    .domain([0, 800000])
+    .range(["lightblue", "darkblue"]);
+
+function zoomed() {
+    g.attr(
+        "transform",
+        "translate(" + d3.event.translate + ")scale(" + d3.event.scale + ")"
+    );
+}
+
+d3.json("cro.json", function (error, data) {
+
+    if (error) {
+        console.log(error);
+        return;
+    }
+
+    var counties = topojson.feature(
+        data,
+        data.objects.layer1
+    ).features;
+
+    g.selectAll("path")
+        .data(counties)
+        .enter()
+        .append("path")
+        .attr("d", path)
+        .style("fill", function (d) {
+            var value = d.properties.population;
+
+            if (value) {
+                return color(value);
+            }
+
+            return "gray";
+        })
+        .style("stroke", "white")
+        .style("stroke-width", 1)
+        .on("mouseover", function (d) {
+
+            tooltip
+                .style("opacity", 1)
+                .html(d.properties.name)
+                .style("left", (d3.event.pageX + 10) + "px")
+                .style("top", (d3.event.pageY - 20) + "px");
+        })
+        .on("mouseout", function () {
+
+            tooltip.style("opacity", 0);
+        })
+        .on("click", clicked);
+});
+
+var resetTimer;
+
+function clicked(d) {
+    var x, y, k;
+
+    if (d && centered !== d) {
+        var centroid = path.centroid(d);
+
+        x = centroid[0];
+        y = centroid[1];
+        k = 4;
+
+        centered = d;
+
+        details.html(
+            "<h2>" + d.properties.name + "</h2>" +
+            "<p>Broj stanovika: " + d.properties.population + "</p>"
+        );
+
+        clearTimeout(resetTimer);
+
+        resetTimer = setTimeout(() => {
+            centered = null;
+
+            g.transition()
+                .duration(1000)
+                .attr('transform', "translate(0,0)scale(1)");
+
+            details.html("");
+        }, 10000);
+    } else {
+        x = width / 2;
+        y = height / 2;
+        k = 1;
+
+        centered = null;
+    }
+
+    g.transition()
+        .duration(1000)
+        .attr(
+            "transform",
+            "translate(" + width / 2 + "," + height / 2 + ")" +
+            "scale(" + k + ")" +
+            "translate(" + -x + "," + -y + ")"
+        );
+}
